e4808f7fcc20078d7e4207a935252e9184634133
Alexander <emotionless013@mail.ru>
2022-08-21 14:02:49 +0400

    hw2.4

---
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -0,0 +1,11 @@
+package pro.sky.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public String welcome() {
+        return "Добро пожаловать в калькулятор!";
+    }
+}
