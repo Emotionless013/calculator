a1effe63d16525949e3fb41501708634f9ddda7d
Alexander <emotionless013@mail.ru>
2022-08-21 15:01:15 +0400

    hw2.4

--- src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/CalculatorServiceImpl.java
@@ -8,4 +8,32 @@ public class CalculatorServiceImpl implements CalculatorService {
     public String welcome() {
         return "Добро пожаловать в калькулятор!";
     }
+
+    @Override
+    public String plus(int num1, int num2) {
+        int result = num1 + num2;
+        return num1 + " + " + num2 + " = " + result;
+    }
+
+    @Override
+    public String minus(int num1, int num2) {
+        int result = num1 - num2;
+        return num1 + " - " + num2 + " = " + result;
+    }
+
+    @Override
+    public String multiply(int num1, int num2) {
+        int result = num1 * num2;
+        return num1 + " * " + num2 + " = " + result;
+    }
+
+    @Override
+    public String divide(double num1, double num2) {
+        if (num2 == 0) {
+            return "Деление на 0 недопустимо!";
+        } else {
+            double result = num1 / num2;
+            return num1 + " / " + num2 + " = " + result;
+        }
+    }
 }
